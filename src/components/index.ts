57cda66022fe75ecccadb8ea335d940cae87960e
Andrii Valenko <andreyvalenko@gmail.com>
2024-05-16 15:29:54 +0300

    save

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
-export { default as Logo } from "../fonts/Logo/Logo";
+export { default as Logo } from "./Logo/Logo";
 export { default as Modal } from "./Modal/Modal";
-export { default as Icon } from "../fonts/Icon/Icon";
+export { default as Icon } from "./Icon/Icon";
 export { default as UIButton } from "./UIButton/UIButton";
 export { default as Pagination } from "./Pagination/Pagination";
 
-export { SearchField } from "../fonts/Fields";
+export { SearchField } from "./Fields";
