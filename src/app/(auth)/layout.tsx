8ad8c8490b23a1a4540dd65d5e65cb375575add9
Andrii Valenko <andreyvalenko@gmail.com>
2024-05-21 13:55:55 +0300

    save

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React, { ReactNode } from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+import styles from "./auth.module.scss";
+
+function AuthLayout({ children }: { children: ReactNode }) {
+  const pathName = usePathname();
+
+  const isRegister = pathName === "/register";
+  return (
+    <main>
+      <section className="section">
+        <div className="container">
+          <div className={styles["auth"]}>
+            <h1 className={styles["auth__title"]}>
+              {isRegister ? "Registration" : "Login"}
+            </h1>
+            {children}
+            <p className={styles["auth__text"]}>
+              Already have an account?{" "}
+              <Link
+                href={isRegister ? "/login" : "/register"}
+                className={styles["link"]}
+              >
+                {isRegister ? "Login" : "Register"}
+              </Link>
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default AuthLayout;
--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,17 +1,23 @@
 "use client";
-import React from "react";
 import Image from "next/image";
 
 import { UIButton } from "@/components";
 import { IconEnum } from "@/types";
+
 import styles from "./app.module.scss";
 
-function NotFound() {
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
   return (
     <main>
       <section className={styles["not-found"]}>
         <div className={`container ${styles["not-found__wrapper"]}`}>
-          <h1>Ooops! This page not found :(</h1>
+          <h1>{error.message}</h1>
           <Image
             alt="Not found"
             src="/images/404-desc@2x.webp"
@@ -20,14 +26,14 @@ function NotFound() {
           />
           <div className={styles["not-found__link"]}>
             <UIButton
-              href="/"
+              onClick={() => reset()}
               icon={IconEnum.PET}
               variant="contained"
               color="secondary"
               alignIcon="right"
               fullWidth
             >
-              To main page
+              Reload
             </UIButton>
           </div>
         </div>
@@ -35,5 +41,3 @@ function NotFound() {
     </main>
   );
 }
-
-export default NotFound;
