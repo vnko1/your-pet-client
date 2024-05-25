2d215ae840d80db1d6ed232c348ea9c47c3b5546
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-05-25 17:45:51 +0300

    save

--- src/hooks/useModal/useModal.ts
+++ src/hooks/useModal/useModal.ts
@@ -7,18 +7,13 @@ export const useModal = (timeout = 300, disableScroll = false) => {
   const [visible, setVisible] = useState(false);
 
   useEffect(() => {
-    if (active) {
-      disableScroll && document.body.classList.add("no-scroll");
-    }
-
-    return () => {
-      document.body.classList.remove("no-scroll");
-    };
+    if (active && disableScroll) document.body.classList.add("no-scroll");
   }, [active, disableScroll]);
 
   const close = () => {
     setVisible(false);
     setTimeout(() => setActive(false), timeout);
+    document.body.classList.remove("no-scroll");
   };
 
   return { close, setActive, setVisible, visible, active };
